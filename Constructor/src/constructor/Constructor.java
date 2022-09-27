052f515236026db6150e7c353bc4bbca6187ad7f
Arvin Situmorang <Arvin Situmorang@P1J92D9K-ARVIN-TUF-GAMING>
2022-09-27 08:15:11 +0700

    initial commit

---
+++ Constructor/src/constructor/Constructor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructor;
+
+/**
+ *
+ * @author Arvin Situmorang
+ */
+public class Constructor {
+    public static void main(String[] args) {
+        System.out.println("Ini adalah constructor pertama!");
+    }
+}
